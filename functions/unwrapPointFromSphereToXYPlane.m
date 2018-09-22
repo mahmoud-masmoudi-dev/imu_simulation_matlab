cc536786a1930b9fb7ecbf0dd2118bd5d2f96c2e
Unknown <mahmoud.masmoudi.dev@gmail.com>
2018-09-22 15:41:36 +0200

    Added stampSphericalData
    Minor fixes

--- functions/unwrapPointFromSphereToXYPlane.m
+++ functions/unwrapPointFromSphereToXYPlane.m
@@ -12,16 +12,17 @@ z = M(3);
 
 % Handle the case of [0 0 1] and [0 0 -1]
 if (dot(M, [0 0 1]) > 0.99)
-    coords2d = [0 0];
+    coords2d = [0; 0];
     return;
 elseif (dot(M, [0 0 1]) < -0.99)
-    coords2d = [pi 0];
+    coords2d = [pi; 0];
     warning('Or maybe [0 pi] ??');
     return;
 end
+
+coords2d = NaN(2, 1);
 coords2d(2) = acos(z)*sin(acos(x/sqrt(1-z^2))*sign(y));
 coords2d(1) = acos(z)*cos(acos(x/sqrt(1-z^2))*sign(y));
-
 
 end
 
